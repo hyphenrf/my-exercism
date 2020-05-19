8aa60de4bd9c4f8a9470e827783beeaa29b92d9f
Hazem <hyphens@pm.me>
2020-05-19 11:44:26 +0200

    went for single-pass

--- ocaml/hamming/hamming.ml
+++ ocaml/hamming/hamming.ml
@@ -6,24 +6,12 @@ let hamming_distance l r =
   | [], [] -> Ok 0
   | [], _  -> Error "left strand must not be empty"
   | _ , [] -> Error "right strand must not be empty"
-  | _ , _  -> let ne a b = if a <> b then 1 else 0 in
+  | _ , _  ->
+    let ne a b = if a <> b then 1 else 0 in
     let rec count ?(n=0) = function
       | [], [] -> n
       | x::xs, y::ys -> count ~n:(n + (ne x y)) (xs, ys)
       | _ -> raise (Failure "left and right strands must be of equal length")
-    in try
-      Ok (count (l, r))
-    with Failure msg ->
-      Error msg
-
-(* With composition -- OCaml is eager, so this is probably more expensive? *)
-let hamming_composed l r = match l, r with
-  | [], [] -> Ok 0
-  | [], _  -> Error "left strand must not be empty"
-  | _ , [] -> Error "right strand must not be empty"
-  | _ , _  -> 
-    try Ok 
-      (  List.combine l r
-      |> List.filter (fun (x, y) -> x <> y)
-      |> List.length )
-    with _ -> Error "left and right strands must be of equal length"
+    in
+    try Ok (count (l, r))
+    with Failure msg -> Error msg
