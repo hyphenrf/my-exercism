cce9d427f4301235d720f8834f81e00018509d6f
Hazem <hyphens@pm.me>
2020-07-08 20:17:37 +0200

    functional style word count

--- ocaml/word-count/word_count.ml
+++ ocaml/word-count/word_count.ml
@@ -1,37 +1,14 @@
-module Map = Base.Map
-module String = Base.String
+open Base
 
-let is_wordchar = function
-    | 'a'..'z'
-    | '0'..'9' -> true
-    | _        -> false
+let sym_strip s = match 
+    String.strip s ~drop:(fun c -> not @@ Char.is_alphanum c)
+    with | "" -> None
+         | cs -> Some cs
 
-let is_apos l m r = 
-    m = '\''
-    && is_wordchar l
-    && is_wordchar r
+let bump = Map.update ~f:(function None -> 1 | Some n -> 1+n)
 
-let bump map word =
-    let keyget = match Map.find map word with
-        | Some n -> n
-        | None   -> 0
-    in
-    Map.set map ~key:word ~data:(keyget + 1)
-
-
-let word_count s =
-    let s = "==" ^ (String.lowercase s) ^ "==" in
-    let smap = ref (Map.empty (module String)) in
-    let stmp = ref "" in
-
-    for i = 1 to (String.length s) - 2 do
-
-        let l, m, r = s.[i-1], s.[i], s.[i+1] 
-        in
-        if is_wordchar m || is_apos l m r
-        then stmp := (!stmp ^ String.make 1 m)
-        else match !stmp with
-             | "" -> ()
-             | _  -> smap := bump !smap !stmp;
-                     stmp := ""
-    done; !smap
+let word_count s = s
+    |> String.lowercase
+    |> String.split_on_chars ~on:['\n';' ';',']
+    |> List.filter_map ~f:sym_strip
+    |> List.fold ~f:bump ~init:(Map.empty (module String))
