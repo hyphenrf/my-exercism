303ddf7d499107cd9720db28deab4d03a0e0e69a
Hazem <hyphens@pm.me>
2020-07-19 20:33:13 +0200

    cleanup, should I just use Str?

--- ocaml/run-length-encoding/run_length_encoding.ml
+++ ocaml/run-length-encoding/run_length_encoding.ml
@@ -1,32 +1,32 @@
 open Base
 
-let encode s =
+let encode s = if String.length s < 2 then s else
     s |> String.to_list
       |> List.group ~break:Char.(<>)
       |> List.map ~f:(fun cs -> (match List.length cs with
-                                    | 1 -> ""
-                                    | n -> Int.to_string n) ^ 
-                                Char.to_string (List.hd_exn cs))
+                                     | 1 -> ""
+                                     | n -> Int.to_string n)
+                                ^ Char.to_string (List.hd_exn cs))
       |> String.concat
 
 
-let decode s = let open Str in
+let decode s = if String.length s < 2 then s else
+    let open Str in
     let rec expand ?(s="") xs =
         match xs with
         | a::b::cs ->
-                begin match a, b with 
-                  (*text = numbers, delim = letter*)
-                  | Text a, Delim b -> 
-                          expand cs ~s:(s^String.make 
-                                          (Int.of_string a) 
-                                          (Char.of_string b))
-                  | Delim a, Text b ->
-                          expand (Text b::cs) ~s:(s^a)
-                  | Delim a, Delim b ->
-                          expand cs ~s:(s^a^b)
-                end
-        | [Text a] | [Delim a] -> s^a
+            begin match a, b with
+              (*text = numbers, delim = letter*)
+              | Delim a, Text b  -> expand (Text b::cs) ~s:(s^a)
+              | Delim a, Delim b -> expand cs ~s:(s^a^b)
+              | Text a,  Delim b -> expand cs ~s:(s^String.make
+                                                    (Int.of_string a)
+                                                    (Char.of_string b))
+              | Text _, Text _ -> failwith "never happens"
+            end
+        | [Delim a] -> s^a
         | [] -> s
+        | [Text _] -> failwith "never happens"
     in
     s |> full_split (regexp_case_fold "[a-z ]")
       |> expand
