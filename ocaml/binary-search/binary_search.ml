a4b65638e64e2f94e2c1d421a11d922f865adf54
Hazem <hyphens@pm.me>
2020-07-19 12:28:33 +0200

    make match truly exhaustive (and less verbose)

--- ocaml/binary-search/binary_search.ml
+++ ocaml/binary-search/binary_search.ml
@@ -3,8 +3,8 @@ let find haystack needle =
         if l > r then Error "value not in array"
         else let i = (l+r)/2 in 
         match haystack.(i) with        
-        | x when x = needle -> Ok i
         | x when x > needle -> search l (i-1)
         | x when x < needle -> search (i+1) r
+        | _ -> Ok i
     in 
     search 0 @@ Array.length haystack - 1
