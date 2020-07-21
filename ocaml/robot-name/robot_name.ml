cc5f2c0dd5201a02cb5d5fcf5c5ecb6da9c6da95
Hazem <hyphens@pm.me>
2020-07-21 14:29:36 +0200

    Records aren't just for data structures
    mutable records are great for encoding program state across function calls

--- ocaml/robot-name/robot_name.ml
+++ ocaml/robot-name/robot_name.ml
@@ -1,23 +1,35 @@
-(* This doesb't work because robot's name never changes once it is assigned 
- * Reset doesn't MUTATE the current robot's name *)
-type robot = string
-module RobotSet = Set.Make (String)
+(**
+ *  This runs as expected and passes all (including optional) tests.
+ *  Mutable records are great for encoding state without passing params
+ *  everywhere.
+ *)
 
-let robots = ref RobotSet.empty
-let () = Random.self_init ()
+module BSet = Set.Make (Bytes)
+type robot = { mutable id: bytes }
 
+let known_ids = ref BSet.empty
+let know id = BSet.mem id !known_ids
+let remove id = BSet.remove id !known_ids
+let remember id = BSet.add id !known_ids
 
 let rec new_robot () =
-    let name =
-        String.init 5 (fun x -> if x < 2
-                      then Char.chr @@ (Random.int 0x1a) + 0x41
-                      else Char.chr @@ (Random.int 0x0a) + 0x30)
+    let rand_id = Bytes.init 5 @@
+        fun x -> if x < 2
+                 then Char.chr @@ (Random.int 26) + 0x41 (*A-Z*)
+                 else Char.chr @@ (Random.int 10) + 0x30 (*0-9*)
+    in 
+    if know rand_id then new_robot()
+    else (known_ids := remember rand_id;
+         {id = rand_id})
+
+let name {id} =
+    Bytes.to_string id
+
+let reset old_robot =
+    (* we need to call new before removing old
+     * to avoid the tiny chance of duplication
+     *)
+    let new_robot = new_robot ()
     in
-    if RobotSet.mem name !robots then new_robot ()
-    else (robots := RobotSet.add name !robots; name)
-
-
-let name (r:robot) = r
-
-let reset robot =
-    robots := RobotSet.remove robot !robots
+    known_ids := remove old_robot.id;
+    old_robot.id <- new_robot.id
