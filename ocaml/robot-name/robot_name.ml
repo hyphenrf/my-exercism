ab53f839709e7626259b773ab6168ae1cf9d0576
Hazem <hyphens@pm.me>
2020-07-21 14:32:23 +0200

    Sets are a BST internally, Hashsets are a mutable table. They're faster

--- ocaml/robot-name/robot_name.ml
+++ ocaml/robot-name/robot_name.ml
@@ -1,35 +1,35 @@
-(**
- *  This runs as expected and passes all (including optional) tests.
+(*  This runs as expected and passes all (including optional) tests.
  *  Mutable records are great for encoding state without passing params
  *  everywhere.
+ *  Hash_set is running 30-50% faster than Set thanks to the fact that it's a
+ *  table not a BST internally. I think it depends on whether or not the set is
+ *  allowed to grow (collisions)
  *)
+open Base
+type robot = { mutable id: string }
 
-module BSet = Set.Make (Bytes)
-type robot = { mutable id: bytes }
+let known_ids = Hash_set.create ~size:(26*26*1000) ~growth_allowed:true
+                (module String)
 
-let known_ids = ref BSet.empty
-let know id = BSet.mem id !known_ids
-let remove id = BSet.remove id !known_ids
-let remember id = BSet.add id !known_ids
+let know = Hash_set.mem known_ids
+let forget = Hash_set.remove known_ids
+let remember = Hash_set.add known_ids
+
 
 let rec new_robot () =
-    let rand_id = Bytes.init 5 @@
-        fun x -> if x < 2
-                 then Char.chr @@ (Random.int 26) + 0x41 (*A-Z*)
-                 else Char.chr @@ (Random.int 10) + 0x30 (*0-9*)
-    in 
+    let chr = Char.unsafe_of_int in
+    let rand_id = String.init 5 ~f:(fun x ->
+        if x < 2 then chr @@ Random.int 26 + 0x41
+                 else chr @@ Random.int 10 + 0x30)
+    in
     if know rand_id then new_robot()
-    else (known_ids := remember rand_id;
+    else (remember rand_id;
          {id = rand_id})
 
-let name {id} =
-    Bytes.to_string id
+let name {id} = id
 
-let reset old_robot =
-    (* we need to call new before removing old
-     * to avoid the tiny chance of duplication
-     *)
-    let new_robot = new_robot ()
-    in
-    known_ids := remove old_robot.id;
+(* we need to call new before removing old to avoid the
+ * tiny chance of duplication *)
+let reset old_robot = let new_robot = new_robot () in
+    forget old_robot.id;
     old_robot.id <- new_robot.id
