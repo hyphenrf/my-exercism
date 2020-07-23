c2a0aef0d4089707cda13809271422c373d223f0
Hazem <hyphens@pm.me>
2020-07-23 02:06:26 +0200

    major cleanup
    used exceptions instead of manual bounds checking with option types

--- ocaml/minesweeper/minesweeper.ml
+++ ocaml/minesweeper/minesweeper.ml
@@ -1,45 +1,32 @@
-let moves = [ (-1,-1); (-1, 0); (-1, 1)
+let getitem (row, col) board =
+    try       Some Bytes.(get board.(row) col)
+    with _ -> None
+
+let cells = [ (-1,-1); (-1, 0); (-1, 1)
             ; (0, -1); (*0, 0*) (0,  1)
-            ; (1, -1); (1,  0); (1,  1)
-            ]
+            ; (1, -1); (1,  0); (1,  1) ]
 
-let get row col board = 
-    let (@:) a ar =
-        if a < 0 || a >= Array.length ar  then None 
-        else Some ar.(a)
-    in
-    let (@.) a st =
-        if a < 0 || a >= Bytes.length st then None
-        else Some (Bytes.get st a)
-    in
-    let (|=) = Option.bind in
-    
-    row @: board |= (@.) col
-
-let bos = Bytes.of_string
 let bump = function
-    | '1'..'7' as a -> Char.chr @@int_of_char a + 1
+    | '1'..'7' as c -> Char.(chr (code c + 1))
     | ' ' -> '1'
-    | a -> a
+    | c -> c
 
-let count row col board =
-    let surrounding (r, c) =
-        match get (r+row) (c+col) !board with
-        | Some a -> !board.(r+row).[c+col] <- (bump a) 
-        | None -> ()
+let annotate ls =
+    let board = ls |> Array.of_list
+                   |> Array.map Bytes.of_string
     in
-    List.iter surrounding moves; !board
+    let flag rmine cmine =
+        let surround (r, c) =
+            match getitem (r+rmine, c+cmine) board with
+            | Some a ->
+                board.(r+rmine).[c+cmine] <- bump a
+            | None -> ()
+        in
+        List.iter surround cells
+    in
+    board |> Array.iteri
+             (fun r -> Bytes.iteri
+             (fun c chr -> if chr = '*' then flag r c));
 
-
-
-let annotate = function
-    | [] -> [] 
-    | [a] when String.length a < 2 -> [a]
-    | initial ->
-        let board = initial |> Array.of_list |> Array.map bos |> ref in
-        
-        !board |> Array.iteri (fun r str -> BytesLabels.iteri str
-                          ~f:(fun c chr -> 
-                              if chr = '*' 
-                              then ignore (count r c board)));
-        !board |> Array.to_list |> List.map Bytes.to_string
+             board |> Array.map Bytes.to_string
+                   |> Array.to_list
