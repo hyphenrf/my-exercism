17a5797216ad1f60d1b067fcf477ca5c3209b510
Hazem <hyphens@pm.me>
2021-04-24 21:33:42 +0200

    turned imperative stream into functional match
    can it be reduced further? probably. I don't know yet.
    I hate the if conditions in the roll function.

--- ocaml/bowling/bowling.ml
+++ ocaml/bowling/bowling.ml
@@ -1,64 +1,41 @@
-type t = { game: int list
-         ; bowl: int
-         ; frame: int
-         }
+type t = { game: int list; bowl: int; frame: int }
 
 let new_game = { game = []; bowl = 0; frame = 0 }
 
+let last2 xs =
+  (try List.hd xs with _ -> 0),
+  (try List.(hd @@ tl xs) with _ -> 0)
+
 let roll pins {game;bowl;frame} =
-  if frame = 9 && bowl > 2 then
-    Error "Cannot roll after game is over"
-  else if frame > 9 then
-    Error "Cannot roll after game is over"
-  else if frame = 9 && bowl = 2 && List.(hd game + hd (tl game) < 10) then
-    Error "Cannot roll after game is over"
-  else if pins < 0 then
+  if pins < 0 then
     Error "Negative roll is invalid"
-  else if pins > 10 then
+  else
+  let x, y = last2 game in
+  let game = pins :: game in
+
+  if frame > 9 || bowl > 2 || frame = 9 && bowl = 2 && x + y < 10 then
+    Error "Cannot roll after game is over"
+  else if pins > 10 || frame < 9 && bowl > 0 && x + pins > 10 || frame = 9 && bowl = 2 && x < 10 && x + pins > 10 && y = 10 then
     Error "Pin count exceeds pins on the lane"
-  else if frame < 9 && bowl > 0 && List.hd game + pins > 10 then
-    Error "Pin count exceeds pins on the lane"
-  else if frame = 9 && bowl = 2 && List.hd game < 10 && List.hd game + pins > 10
-  && List.(hd @@ tl game) = 10 then
-    Error "Pin count exceeds pins on the lane"
-  else begin
-    let game = pins :: game in
+  else
     let frame, bowl =
-      if frame < 9 then
-        begin
-        if pins = 10 && bowl = 0 then
-          frame + 1, bowl
-        else
-          frame + bowl, succ bowl mod 2
-        end
-      else
-        frame, succ bowl
+      if frame = 9 then frame, succ bowl
+      else if pins = 10 && bowl = 0 then frame+1, 0
+      else frame + bowl, succ bowl mod 2
     in
-    Ok {frame;bowl;game} 
-  end
-    
-let score {game;frame;bowl} = 
-  if frame < 9 then
-    Error "Score cannot be taken until the end of the game"
-  else if frame = 9 && bowl < 3 && List.(hd game + hd (tl game) >= 10) then
-    Error "Score cannot be taken until the end of the game"
-  else begin
-    let open Stream in
-    let s = of_list @@ List.rev game in
-    let score = ref 0 in
-    for _ = 0 to 9 do
-      let x = next s in
-      if x = 10 then (* strike *)
-        let j = npeek 2 s |> List.fold_left (+) 0 in
-        score := !score + x + j
-      else begin
-        let j = next s in
-        if x + j = 10 then (* spare *)
-          let k = peek s |> Option.value ~default:0 in
-          score := !score + x + j + k
-        else (* open *)
-          score := !score + x + j
-      end
-    done;
-    Ok !score
-  end
+    Ok {frame;bowl;game}
+
+let score {game;_} =
+  let rec aux (frame, score) =
+    if frame < 11 then function
+    | x::(y::z::_ as rest) when x = 10 -> aux (frame+1, score + 10 + y + z) rest
+    | x::y::(z::_ as rest) when x + y = 10 -> aux (frame+1, score + 10 + z) rest
+    | x::y::rest when x + y < 10 -> aux (frame+1, score + x + y) rest
+    | _::_ ->
+        Error "Score cannot be taken until the end of the game"
+    | [] when frame < 11 ->
+        Error "Score cannot be taken until the end of the game"
+    | [] ->
+        Ok score
+    else Fun.const (Ok score)
+  in aux (1, 0) (List.rev game)
